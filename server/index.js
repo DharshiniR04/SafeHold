84fafaaa2f3a150d55637cd9ba57bec03745acff
Dharshini <2112026@nec.edu.in>
2024-08-27 21:54:29 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
-const serverless = require('serverless-http');
-const app = require('./server'); 
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
-module.exports.handler = serverless(app);
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json("WELCOME TO SAFEHOLD");
+});
+
+app.use('/api/auth', authRoutes);
+
+app.listen(process.env.PORT,()=>{console.log("Running on port 5000")});
+
